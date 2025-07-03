1b5ba8cc5e627ddb8c6b33f2871a04c77fa4cc27
KasraHosseini <Kasra.hosseini.0013@gmail.com>
2025-07-03 18:34:30 +0330

    Add files via upload

--- adminPage/connection.js
+++ adminPage/connection.js
@@ -1,99 +1,3 @@
-// let currentMode = "";
-// let p_id;
-// let p_img;
-// let p_title;
-// let p_desc;
-// let p_price;
-// let p_sale;
-// let p_cate;
-// let p_delId;
-// document.querySelector(".sub-btn").addEventListener("click", () => {
-//   document.querySelector(".msg").style.bottom = "0";
-//   document.querySelector(".msg").style.boxShadow = "0 0 50px black";
-//   p_id = Number(document.querySelector("#p_id").value);
-//   p_img = document.querySelector("#p_img").value;
-//   p_title = document.querySelector("#p_title").value;
-//   p_desc = document.querySelector("#p_desc").value;
-//   p_price = Number(document.querySelector("#p_price").value);
-//   p_sale = Number(document.querySelector("#p_sale").value);
-//   p_cate = document.querySelector("#p_cate").value;
-//   p_delId = document.querySelector("#delId").value;
-//   console.log(p_id);
-//   console.log(p_img);
-//   document.querySelector('button[data-progress="true"]').addEventListener("click", () => {
-//     document.querySelector(".msg").style.bottom = "-138px";
-//     document.querySelector(".msg").style.boxShadow = "0 0 0 black";
-//     changeData(p_id, p_img, p_title, p_desc, p_price, p_sale, p_cate);
-//   });
-//   document.querySelector('button[data-progress="false"]').addEventListener("click", () => {
-//     document.querySelector(".msg").style.bottom = "-138px";
-//     document.querySelector(".msg").style.boxShadow = "0 0 0 black";
-//   });
-// });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   document.querySelector("#firstAction").addEventListener("click", (e) => {
-//     if (e.target != document.querySelector("input[type='number']")) {
-//       document.querySelector("#actions").style.height = "132px";
-//       document.querySelector("#actions").style.zIndex = "3";
-//     }
-//   });
-//   [...document.querySelectorAll("#actions div")].map((elem) => {
-//     elem.addEventListener("click", () => {
-//       currentMode = elem.getAttribute("data-admin-mode");
-//       document.querySelector("#firstAction").innerHTML = elem.innerHTML;
-//       document.querySelector("#actions").style.height = "44px";
-//       document.querySelector("#actions").style.zIndex = "1";
-//     });
-//   });
-// });
-// async function changeData(p_id, p_img, p_title, p_desc, p_price, p_sale, p_cate) {
-//   let data = {
-//     id: p_id,
-//     p_img,
-//     p_title,
-//     p_desc,
-//     p_price,
-//     p_sale,
-//     p_cate,
-//   };
-//   try {
-//     const res = await fetch("https://67964d45bedc5d43a6c4db00.mockapi.io/api/db1/data/1", {
-//       method: "GET",
-//     });
-//     if (!res.ok) {
-//       alert(`http error: ${res.status}`);
-//       throw new Error(`http error: ${res.status}`);
-//     }
-//     let receivedData = await res.json();
-//     if (currentMode == "addData") {
-//       receivedData.db.product.product.unshift(data);
-//     } else if (currentMode == "editData") {
-//       receivedData.db.product.product = receivedData.db.product.product.map((elem) => (elem.id == p_id ? data : elem));
-//     } else {
-//       receivedData.db.product.product = receivedData.db.product.product.map((elem) => {
-//         if (elem.id == p_delId) {
-//         }
-//       });
-//     }
-//     const action = await fetch("https://67964d45bedc5d43a6c4db00.mockapi.io/api/db1/data/1", {
-//       method: "PUT",
-//       headers: {
-//         "Content-type": "application/json",
-//       },
-//       body: JSON.stringify(receivedData),
-//     });
-//     if (!action.ok) {
-//       alert(`http error: ${res.status}`);
-//       throw new Error(`http error: ${res.status}`);
-//     }
-//   } catch (err) {
-//     alert(`fetch error: ${err.message}`);
-//     console.error(`fetch error: ${err.message}`);
-//   }
-// }
-// connection.js
-
 const API = "https://67964d45bedc5d43a6c4db00.mockapi.io/api/db1/data/1";
 let mode = "";
 
@@ -111,7 +15,6 @@ const inputs = {
   price: document.querySelector("#p_price"),
   sale: document.querySelector("#p_sale"),
   cate: document.querySelector("#p_cate"),
-  delId: document.querySelector("#delId"),
 };
 
 firstBtn.addEventListener("click", (e) => {
@@ -137,7 +40,7 @@ msgBox.addEventListener("click", async (e) => {
   msgBox.style.cssText = "bottom:-138px; box-shadow:none;";
 
   if (e.target.dataset.progress === "true") {
-    const item = {
+    let item = {
       id: +inputs.id.value,
       img_url: inputs.img.value,
       title: inputs.title.value,
@@ -147,12 +50,11 @@ msgBox.addEventListener("click", async (e) => {
       sale: +inputs.sale.value,
       cate: inputs.cate.value,
     };
-    const delId = +inputs.delId.value;
-    await syncData(item, delId);
+    await syncData(item);
   }
 });
 
-async function syncData(item, delId) {
+async function syncData(item) {
   try {
     const res = await fetch(API);
     if (!res.ok) throw new Error(`GET ${res.status}`);
@@ -167,7 +69,7 @@ async function syncData(item, delId) {
         list = list.map((i) => (i.id === item.id ? item : i));
         break;
       case "deleteData":
-        list = list.filter((i) => i.id == delId);
+        list = list.filter((i) => i.id !== item.id);
         break;
       default:
         console.warn("no mode!");
